496f87b8dd970b60afbc6ea680400b9ac2cdf235
hellbrandt <hellbrandt@googlemail.com>
2021-02-11 16:50:29 +0000

    Switch pages and link with Link component 195 196

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 // import Post from '../../components/Post/Post';
 // import FullPost from './FullPost/FullPost';
 // import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -15,14 +16,19 @@ class Blog extends Component {
                 <header className="Blog">
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
